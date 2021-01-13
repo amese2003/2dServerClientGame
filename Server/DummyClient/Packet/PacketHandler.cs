043cd50a46ea772fdf19578252ffd90a5f402dc6
Yong Ju Kwak <amese1225@gmail.com>
2021-01-13 16:25:41 +0900

    # inital commit

---
+++ Server/DummyClient/Packet/PacketHandler.cs
@@ -0,0 +1,57 @@
+﻿using ServerCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DummyClient;
+using ServerCore;
+
+
+class PacketHandler
+{
+
+    //public static void S_ChatHandler(PacketSession session, IPacket packet)
+    //{
+    //    S_Chat chatPacket = packet as S_Chat;
+    //    ServerSession serverSession = session as ServerSession;
+
+    //    //if(chatPacket.playerId == 1)
+    //        //Console.WriteLine(chatPacket.chat);
+    //}
+
+    public static void S_BroadcastEnterGameHandler(PacketSession session, IPacket packet)
+    {
+        S_BroadcastEnterGame enterPacket = packet as S_BroadcastEnterGame;
+        ServerSession serverSession = session as ServerSession;
+
+        //if(chatPacket.playerId == 1)
+        //Console.WriteLine(chatPacket.chat);
+    }
+
+    public static void S_BroadcastLeaveGameHandler(PacketSession session, IPacket packet)
+    {
+        S_BroadcastLeaveGame leavePacket = packet as S_BroadcastLeaveGame;
+        ServerSession serverSession = session as ServerSession;
+
+        //if(chatPacket.playerId == 1)
+        //Console.WriteLine(chatPacket.chat);
+    }
+
+    public static void S_BroadcastMoveHandler(PacketSession session, IPacket packet)
+    {
+        S_BroadcastMove movePacket = packet as S_BroadcastMove;
+        ServerSession serverSession = session as ServerSession;
+
+        //if(chatPacket.playerId == 1)
+        //Console.WriteLine(chatPacket.chat);
+    }
+
+    public static void S_PlayerListHandler(PacketSession session, IPacket packet)
+    {
+        S_PlayerList pkt = packet as S_PlayerList;
+        ServerSession serverSession = session as ServerSession;
+
+        //if(chatPacket.playerId == 1)
+        //Console.WriteLine(chatPacket.chat);
+    }
+}
+
